000308cc95efc7a5222ceab7e24306e091fb486e
nick-pestell <np0877@bristol.ac.uk>
2021-11-24 19:51:31 +0000

    extract last column

--- bwt.py
+++ bwt.py
@@ -1,10 +1,5 @@
 import argparse
 
-def bwt(in_string):
-	in_table = [in_string]
-	table = build_table(in_table)
-	return table
-
 def build_table(in_list):
 	if len(in_list)<len(in_list[0]):
 		in_list.append(in_list[-1][-1]+in_list[-1][0:-1])
@@ -15,16 +10,24 @@ def sort_table(table):
 	table.sort()
 	return table
 
+def extract_last_column(table):
+	return ''.join([table[i][-1] for i in range(len(table))])
+
+def bwt(in_string):
+	in_table = [in_string]
+	table = build_table(in_table)
+	sorted_table = sort_table(table)
+	return extract_last_column(sorted_table)
+
 def run(in_file_path, out_file_path):
 	try:
 		with open(in_file_path, 'r') as fd:
 			in_string = fd.read().rstrip()
 	except FileNotFoundError as e:
 		print(e)
-	table = bwt(in_string)
-	sorted_table = sort_table(table)
+	output = bwt(in_string)
 	with open(out_file_path, 'w') as fd:
-		fd.write('\n'.join(sorted_table))	
+		fd.write(output)	
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Burrows-Wheeler transfrom')
