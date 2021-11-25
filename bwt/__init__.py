244963463ea325b5f543d63c3d8dbcf3988691da
nick-pestell <np0877@bristol.ac.uk>
2021-11-25 09:05:32 +0000

    docstrings

---
+++ bwt/__init__.py
@@ -0,0 +1,16 @@
+'''
+Package for performing Burrows-Wheeler transform operations
+
+    Modules:
+        bwt.transform: perform foward bwt transform
+        
+            Functions:
+                build_table
+                sort_table
+                extract_last_column
+                bwt
+
+        bwt.inv_transform: perform inverse-bwt transform
+
+            Functions:
+'''
--- bwt/transform.py
+++ bwt/transform.py
@@ -1,21 +1,59 @@
+'''
+Module for performing forward Burrows-Wheeler transforms on strings
+'''
 def build_table(in_list):
+	'''
+	Returns a table (list) of strings where each element has character contents rotated by 1
+
+		Parameters:
+			in_list (list): of length 1, contains string to be transformed
+
+		Returns:
+			in_list (list): of lenght len(in_list), table of rotated in_list
+	'''
 	if len(in_list)<len(in_list[0]):
 		in_list.append(in_list[-1][-1]+in_list[-1][0:-1])
 		build_table(in_list)
 	return in_list	
 
 def sort_table(table):
+	'''
+	Lexically sorts a table (list)
+
+		Parameters:
+			table (list): a list of strings to be lexically sorted
+
+		Returns:
+			table (list): a lexically sorted list of strings
+	'''
 	table.sort()
 	return table
 
 def extract_last_column(table):
-	return ''.join([table[i][-1] for i in range(len(table))])
+	'''
+	Returns last column of table 
+
+		Parameters:
+			table (list): a list of strings
+
+		Returns:
+			last_columns (string): last characters of each element within table
+	'''
+	last_column = ''.join([table[i][-1] for i in range(len(table))]) 
+	return last_column
 
 def bwt(in_string):
+	'''
+	Performs Burrows-Wheeler transform
+
+		Parameters:
+			in_string (string): string to be transformed
+
+		Returns:
+			output (string): transformed string
+	'''
 	in_table = [in_string]
 	table = build_table(in_table)
 	sorted_table = sort_table(table)
-	return extract_last_column(sorted_table)
-
-def i_btw(out_string):
-	pass
+	output = extract_last_column(sorted_table) 
+	return output 
