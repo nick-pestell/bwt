a83f59684533933f19656caf5bf35088b0de3f33
nick-pestell <np0877@bristol.ac.uk>
2021-11-26 20:03:04 +0000

    start inverse transform

--- bwt/inv_transform.py
+++ bwt/inv_transform.py
@@ -1,2 +1,15 @@
-def inv_bwt():
-    pass
+# TODO parse table back in to recursive function is the whole
+# table but needs to be just the bwt transformed message each 
+# recurssive call
+def build_table(table):
+    if len(table[0])<len(table):
+        sorted_table = sorted(table)
+        for i in range(len(sorted_table)):
+            sorted_table[i]+=table[i]
+        build_table(sorted_table)
+    return sorted_table
+
+def inv_bwt(in_string):
+    table = list(in_string)
+    output = build_table(table)
+    return output
--- inv_bwt.py
+++ inv_bwt.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import errno
 
-from bwt.transform import bwt
+from bwt.inv_transform import inv_bwt
 
 def run(in_file_path, out_file_path):
 	try:
@@ -11,12 +11,12 @@ def run(in_file_path, out_file_path):
 	except FileNotFoundError as e:
 		print(e)
 		sys.exit(int(errno.ENOENT))
-	output = bwt(in_string)
+	output = inv_bwt(in_string)
 	with open(out_file_path, 'w') as fd:
 		fd.write(output)	
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Burrows-Wheeler transfrom')
+	parser = argparse.ArgumentParser(description='Inverse Burrows-Wheeler transfrom')
 	parser.add_argument('in_file_path', metavar='Input file path', 
 						help='path to input file')
 	parser.add_argument('out_file_path', metavar='Output file path', 
