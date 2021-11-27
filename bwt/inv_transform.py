faeee54bcef7c9a9049fae5e12e348f9f0d76cc5
nick-pestell <np0877@bristol.ac.uk>
2021-11-27 20:49:13 +0000

    build table

--- bwt/inv_transform.py
+++ bwt/inv_transform.py
@@ -1,15 +1,9 @@
-# TODO parse table back in to recursive function is the whole
-# table but needs to be just the bwt transformed message each 
-# recurssive call
-def build_table(table):
-    if len(table[0])<len(table):
-        sorted_table = sorted(table)
-        for i in range(len(sorted_table)):
-            sorted_table[i]+=table[i]
-        build_table(sorted_table)
-    return sorted_table
+def build_table(in_string):
+    table = ['']*len(in_string)
+    for i in range(len(in_string)):
+        table = sorted([table[i] + in_string[i] for i in range(len(in_string))])
+    return table
 
 def inv_bwt(in_string):
-    table = list(in_string)
-    output = build_table(table)
+    output = build_table(in_string)
     return output
