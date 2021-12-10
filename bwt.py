7f2ee7ee2f96190e36d0e562ca5dd9b1509e0090
nick-pestell <np0877@bristol.ac.uk>
2021-12-10 15:18:16 +0000

    condense

--- bwt.py
+++ bwt.py
@@ -3,8 +3,9 @@ import sys
 import errno
 
 from bwt.transform import bwt
+from bwt.inv_transform import inv_bwt
 
-def run(in_file_path, out_file_path):
+def run_forward(in_file_path, out_file_path):
 	try:
 		with open(in_file_path, 'r') as fd:
 			in_string = fd.read()
@@ -15,11 +16,25 @@ def run(in_file_path, out_file_path):
 	with open(out_file_path, 'w') as fd:
 		fd.write(output)	
 
+def run_inverse(in_file_path, out_file_path):
+	try: 
+		with open(in_file_path, 'r') as fd:
+			in_string = fd.read()
+	except FileNotFoundError as e:
+		print(e)
+		sys.exit(errno.ENOENT)	
+	output = inv_bwt(in_string)
+	with open(out_file_path, 'w') as fd:
+		fd.write(output)	
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Burrows-Wheeler transfrom')
+	parser.add_argument('--inv', dest='run', action='store_const',
+						const=run_inverse, default=run_forward,
+						help='run inverse BWT (default: run fordwars BWT')
 	parser.add_argument('in_file_path', metavar='Input file path', 
 						help='path to input file')
 	parser.add_argument('out_file_path', metavar='Output file path', 
 						help='path to output file')
 	args = parser.parse_args()
-	run(args.in_file_path, args.out_file_path)
+	args.run(args.in_file_path, args.out_file_path)
