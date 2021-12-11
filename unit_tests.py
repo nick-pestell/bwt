5b02ec18d69bc6d26e401af7b3ddb970601eb08b
nick-pestell <np0877@bristol.ac.uk>
2021-12-11 11:57:40 +0000

    debugging

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import pickle as pkl
 
 from bwt.transform import *
 from bwt.inv_transform import *
@@ -19,29 +20,32 @@ TEST_OUT_TABLE = ['\n\x7fhello world!',
                   'rld!\n\x7fhello wo', 
                   'world!\n\x7fhello ', 
                   '\x7fhello world!\n']
-IN_FILE_PATH = 'test_files/test.txt'
-OUT_FILE_PATH = 'test_files/test_transformed.txt'
+IN_FILE_PATH = './test_files/turing.txt'
+OUT_FILE_PATH = './test_files/out_turing.txt'
+TABLE_FILE_PATH = './test_files/table.pkl'
 
 class TestBwtTransform(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestBwtTransform, self).__init__(*args, **kwargs)
-        with open('test_files/un-transformed/test.txt', 'w') as fd:
+        with open(IN_FILE_PATH, 'w') as fd:
             self.in_string = fd.read()
-        with open('test_fies/transformed/test_transformed.txt', 'w' ) as fd:
+        with open(OUT_FILE_PATH, 'w' ) as fd:
             self.out_string = fd.read()
+        with open(TABLE_FILE_PATH, 'rb') as fd:
+            self.table = pkl.load(fd)
 
     def test_build_transform_table(self):
-        self.assertEqual(len(TEST_IN_STRING), len(build_transform_table([TEST_IN_STRING]))) 
+        self.assertListEqual(self.table, build_transform_table([self.in_string])) 
         
     def test_bwt(self):
-        self.assertEqual(bwt(TEST_IN_STRING), TEST_OUT_STRING)
+        self.assertEqual(bwt(self.in_string), self.out_string)
 
     def test_build_inv_table(self):
-        self.assertEqual(len(TEST_OUT_STRING), len(build_inv_table(TEST_OUT_STRING)))
+        self.assertEqual(len(self.out_string), len(build_inv_table(self.out_string)))
 
     def test_extract_string(self):
-        self.assertEqual(extract_string(TEST_OUT_TABLE), TEST_IN_STRING)
+        self.assertEqual(extract_string(self.table), self.in_string)
 
     def test_inv_bwt(self):
-        self.assertEqual(inv_bwt(TEST_OUT_STRING), TEST_IN_STRING)
+        self.assertEqual(inv_bwt(self.out_string), self.in_string)
 
