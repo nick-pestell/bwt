6274e56751ae26e14311b8156ae9954c7d225d3b
nick-pestell <np0877@bristol.ac.uk>
2021-12-18 16:47:25 +0000

    make test suits

--- unit_tests.py
+++ unit_tests.py
@@ -1,25 +1,11 @@
 import unittest
 import pickle as pkl
+import argparse
+import errno
 
 from bwt.transform import *
 from bwt.inv_transform import *
 
-TEST_IN_STRING = 'hello world!\n'
-TEST_OUT_STRING = '!odlh\x7frellwo \n'
-TEST_OUT_TABLE = ['\n\x7fhello world!', 
-                  ' world!\n\x7fhello', 
-                  '!\n\x7fhello world', 
-                  'd!\n\x7fhello worl', 
-                  'ello world!\n\x7fh', 
-                  'hello world!\n\x7f', 
-                  'ld!\n\x7fhello wor', 
-                  'llo world!\n\x7fhe', 
-                  'lo world!\n\x7fhel', 
-                  'o world!\n\x7fhell', 
-                  'orld!\n\x7fhello w', 
-                  'rld!\n\x7fhello wo', 
-                  'world!\n\x7fhello ', 
-                  '\x7fhello world!\n']
 IN_FILE_PATH = 'test_files/turing.txt'
 OUT_FILE_PATH = 'test_files/out_turing.txt'
 TABLE_FILE_PATH = 'test_files/table.pkl'
@@ -41,15 +27,49 @@ class TestBwtTransform(unittest.TestCase):
     def test_bwt(self):
         self.assertEqual(bwt(self.in_string), self.out_string)
 
+class TestBwtInverseTransfrom(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestBwtInverseTransfrom, self).__init__(*args, **kwargs)
+        self.maxDiff = None
+        with open(IN_FILE_PATH, 'r') as fd:
+            self.in_string = fd.read()
+        with open(OUT_FILE_PATH, 'r' ) as fd:
+            self.out_string = fd.read()
+        with open(TABLE_FILE_PATH, 'rb') as fd:
+            self.table = pkl.load(fd)
+
     def test_build_inv_table(self):
         self.assertEqual(len(self.out_string + '\x7f'), len(build_inv_table(self.out_string + '\x7f')))
 
     def test_extract_string(self):
         self.assertEqual(extract_string(self.table), self.in_string)
 
-    # TODO: this test is failing now
     def test_inv_bwt(self):
         self.assertEqual(inv_bwt(self.out_string), self.in_string)
 
-if __name__ == "__main__":
-    unittest.main()
+def test_suit(test_objs):
+    suit = unittest.TestSuite(test_objs)
+    return suit
+    
+if __name__ == '__main__':
+    bwt_tests = [TestBwtTransform('test_build_transform_table'), TestBwtTransform('test_bwt')]
+    inv_bwt_test = [TestBwtInverseTransfrom('test_build_inv_table'), 
+                    TestBwtInverseTransfrom('test_extract_string'),
+                    TestBwtInverseTransfrom('test_inv_bwt')]
+    runner = unittest.TextTestRunner()
+    parser = argparse.ArgumentParser(description='Test code')
+    type_arg = parser.add_argument('--type', '-t', nargs=1, 
+                                   help='Forward or inverse transform', default=None)
+    args = parser.parse_args()
+    try:
+        if args.type[0] == 'forward':
+            runner.run(test_suit(bwt_tests)) 
+        elif args.type[0] == 'inverse':
+            runner.run(test_suit(inv_bwt_test)) 
+        elif args.type[0] == None:
+            unittest.main()
+        else:
+            raise argparse.ArgumentError(type_arg, "Invalid argument. Please use 'forward' or 'reverse'")
+    except argparse.ArgumentError as e:
+        print(e)
+        sys.exit(errno.ENOENT)
